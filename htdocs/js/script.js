252caccda40d1d2c4b7d37bf90aa1013a38bab93
IsaiahSloan <103592510+IsaiahSloan@users.noreply.github.com>
2022-05-12 12:55:36 -0500

    v4.2
    - fixed bootstrap, linked in all pages
    - added devlog page
    - removed snake.html
    - changed error.html

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -1,17 +1,4 @@
-function f1() {
-  if(document.all) { return false; }
+function displayPrevious() {
+  var p = document.getElementById("paragraph");
+  p.style.display = "block";
 }
-function f2(e) {
-  if(document.layers || (document.getElementById &! document.all)) {
-    if(e.which==2 || e.which==3) { return false; }
-  }
-}
-if(document.layers) {
-  document.captureEvents(Event.MOUSEDOWN);
-  document.onmousedown = f1;
-}
-else {
-  document.onmouseup = f2;
-  document.oncontextmenu = f1;
-}
-document.oncontextmenu = new function("return false");

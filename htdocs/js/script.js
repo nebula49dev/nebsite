93e7c309a5a0b7bc1bffe90dc90e1585c55ed8ec
IsaiahSloan <103592510+IsaiahSloan@users.noreply.github.com>
2022-05-16 21:40:36 -0500

    v5.0
    huge makover. my assignment is done, so expect much smaller and more infrequent updates. i will still do plenty of devlogs

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -8,45 +8,6 @@ function toggleDiv() {
   }
 }
 
-//create cookie
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-//get cookie
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-//check cookie
-function checkCookie() {
-  let username = getCookie("username");
-  if (username != "") {
-   alert("Welcome again " + username);
-  } else {
-    username = prompt("Please enter your name:", "");
-    if (username != "" && username != null) {
-      setCookie("username", username, 365);
-    }
-  }
-}
-
-//count error 404s
-function cb(response) {
-    document.getElementById('visits').innerText = response.value;
-}
+//display current year at the bottom of the page
+const p = new Date();
+document.getElementById("getYear").innerHTML = p.getFullYear();
